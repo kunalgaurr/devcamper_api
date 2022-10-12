0729f8f2f7f635e33a347aa02f1edd0991772725
kunalgaurr <kunalgaur84@gmail.com>
2022-10-12 12:59:18 +0530

    New changes

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -11,8 +11,8 @@ exports.getBootcamps = async (req, res, next) => {
     res
       .status(200)
       .json({ success: true, count: bootcamps.length, data: bootcamps });
-  } catch (error) {
-    res.status(400).json({ success: false });
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -28,13 +28,11 @@ exports.getBootcamp = async (req, res, next) => {
       );
     }
     res.status(200).json({ success: true, data: bootcamp });
-  } catch (error) {
+  } catch (err) {
     // res.status(400).json({
     //   success: false,
     // });
-    next(
-      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-    );
+    next(err);
   }
 };
 
@@ -49,8 +47,8 @@ exports.createBootcamp = async (req, res, next) => {
       success: true,
       data: bootcamp,
     });
-  } catch (error) {
-    res.status(400).json({ success: false });
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -65,11 +63,13 @@ exports.updateBootcamp = async (req, res, next) => {
     });
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
-  } catch (error) {
-    res.status(400).json({ success: false });
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -81,10 +81,12 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id, req.body);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({ success: true, data: {} });
-  } catch (error) {
-    res.status(400).json({ success: false });
+  } catch (err) {
+    next(err);
   }
 };
--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,16 @@
-const errorHandler = (error, req, res, next) => {
-  // log to console for dev
-  console.log(error.stack.red);
+const ErrorResponse = require('../utils/errorResponse');
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
 
+  error.message = err.message;
+  // log to console for dev
+  console.log(err);
+
+  //   Mongoose bad object id
+  if (err.name === 'CastError') {
+    const message = `Bootcamp not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error',
